d75f293a677445ad66c7c8ea4df4818357cb9251
Frederick Hamon <frederick@deuxhuithuit.co>
2019-11-22 11:18:54 -0500

    added composer stuff

---
+++ content/content.metas.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
+
+require_once(TOOLKIT . '/class.jsonpage.php');
+
+class contentExtensionCloudflare_videosMetas extends JSONPage {
+    
+    public function view() {
+        var_dump('hello world');
+        die();
+        
+        $this->_Result = [];
+    }
+}
